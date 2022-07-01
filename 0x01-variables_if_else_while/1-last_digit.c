7e8b8edc76a45e959148e7cd0eaa0b4ade2d8d3a
Emmanuel <emmanuelolorunmolu@gmail.com>
2022-07-01 04:14:00 -0700

    correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+{
+char letter; /*Decarling statement*/
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+
+putchar('\n');
+
+return (0);
+
 }
