62b5ae7e0dc9afbe6ed43dcddbba1d95356296cd
Emmanuel <emmanuelolorunmolu@gmail.com>
2022-07-01 04:34:30 -0700

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
-
 {
-char letter; /*Decarling statement*/
+int n;
 
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-putchar(letter);
-}
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-putchar('\n');
+if (n % 10 > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+else if (n % 10 == 0)
+printf("Last digit of %d is %d and is 0\n", n, n % 10)
+else
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 
 return (0);
-
 }
