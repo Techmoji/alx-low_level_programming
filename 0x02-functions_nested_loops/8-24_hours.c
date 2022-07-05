3ae230d0daebdc3f1f63abbfba9a95c5129a09c9
Emmanuel <emmanuelolorunmolu@gmail.com>
2022-07-05 13:52:20 -0700

    betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,28 @@
 #include "main.h"
-
 /**
- * jack_bauer - Check School
- * Description: function prints every minute in one day
- * Return: Nothing
+ * jack_bauer -prints every minute of the day of Jack Bauer
+ *
+ * Return: nothing
  */
 void jack_bauer(void)
-{
-int H, M;
 
-for (H = 0; H < 24; H++)
 {
-for (M = 0; M < 60; m++)
+int a;
+int b;
+
+for (a = 0; a <= 23; a++) /*First for*/
 {
-_putchar((H / 10) + '0');
-_putchar((H % 10) + '0');
-_putchar(':');
-_putchar((M / 10) + '0');
-_putchar((M % 10) + '0');
-_putchar('\n');
+for (b = 0; b <= 59; b++) /*Second for*/
+{
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
+_putchar (':');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
+_putchar ('\n');
+
 }
+
 }
+
 }
